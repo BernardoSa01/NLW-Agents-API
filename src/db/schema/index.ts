666819badb1ad1b7ec83e9d1fc00bd530ed30418
bernardo <bernardo_nf@hotmail.com>
2025-07-21 20:42:26 -0300

    feat(audio): add audioChunks schema and upload audio endpoint

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,8 +1,10 @@
+import { audioChunks } from "./audio-chunks.ts";
 import { questions } from "./questions.ts";
 import { rooms } from "./rooms.ts";
 
 // Barrel file
 export const schema = {
   rooms,
-  questions
+  questions,
+  audioChunks
 }
